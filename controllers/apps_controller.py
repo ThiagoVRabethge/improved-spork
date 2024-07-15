7e3688fdeb8297c81ca80eb156d020085ea61418
Thiago Volpi Rabethge~ <thiago.rabethge@gmail.com>
2024-07-15 16:19:58 -0300

    feature: add put app route

--- controllers/apps_controller.py
+++ controllers/apps_controller.py
@@ -4,6 +4,8 @@ from database import engine, create_db_and_tables
 
 from models.apps_model import Apps
 
+from fastapi import FastAPI, HTTPException
+
 
 def post_app(app: Apps):
     with Session(engine) as session:
@@ -11,6 +13,23 @@ def post_app(app: Apps):
         session.commit()
         session.refresh(app)
         return app
+
+
+def put_app(app_id: int, app: Apps):
+    with Session(engine) as session:
+        statement = select(Apps).where(Apps.id == app_id)
+        results = session.exec(statement)
+        db_app = results.one()
+
+        db_app.name = app.name
+        db_app.description = app.description
+        db_app.link = app.link
+
+        session.add(db_app)
+        session.commit()
+        session.refresh(db_app)
+
+        return db_app
 
 
 def delete_app(app_id: int):
--- models/users_model.py
+++ models/users_model.py
@@ -1,7 +1,9 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlalchemy import UniqueConstraint
 
 
 class Users(SQLModel, table=True):
+    __table_args__ = (UniqueConstraint("username"),)
     id: int | None = Field(default=None, primary_key=True)
     username: str = Field(index=True)
     password: str = Field(index=True)
