3a7975d0cb06600ce91a27c85d9205080fc74529
Thiago Volpi Rabethge~ <thiago.rabethge@gmail.com>
2024-09-13 19:26:01 -0300

    teste

--- services/firebase.py
+++ services/firebase.py
@@ -1,3 +1,4 @@
+import json
 import os
 from dotenv import load_dotenv
 import firebase_admin
@@ -6,23 +7,29 @@ from firebase_admin import credentials, storage
 load_dotenv()
 
 
+def load_config():
+    with open("config.json", "r") as json_file:
+        config = json.load(json_file)
+        # Substitui os placeholders pelas variáveis de ambiente
+        config["type"] = os.getenv("type")
+        config["project_id"] = os.getenv("project_id")
+        config["private_key_id"] = os.getenv("private_key_id")
+        config["private_key"] = os.getenv("private_key")
+        config["client_email"] = os.getenv("client_email")
+        config["client_id"] = os.getenv("client_id")
+        config["auth_uri"] = os.getenv("auth_uri")
+        config["token_uri"] = os.getenv("token_uri")
+        config["auth_provider_x509_cert_url"] = os.getenv("auth_provider_x509_cert_url")
+        config["client_x509_cert_url"] = os.getenv("client_x509_cert_url")
+        config["universe_domain"] = os.getenv("universe_domain")
+    return config
+
+
 def initialize_firebase():
     if not firebase_admin._apps:
-        test = {
-            "type": os.getenv("type"),
-            "project_id": os.getenv("project_id"),
-            "private_key_id": os.getenv("private_key_id"),
-            "private_key": os.getenv("private_key"),
-            "client_email": os.getenv("client_email"),
-            "client_id": os.getenv("client_id"),
-            "auth_uri": os.getenv("auth_uri"),
-            "token_uri": os.getenv("token_uri"),
-            "auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"),
-            "client_x509_cert_url": os.getenv("client_x509_cert_url"),
-            "universe_domain": os.getenv("universe_domain"),
-        }
+        config = load_config()
  
-        cred = credentials.Certificate(test)
+        cred = credentials.Certificate(config)
   
         firebase_admin.initialize_app(
             cred,
